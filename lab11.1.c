22f5f30834050e3dec7975f0cf79119741108b44
xhable1337 <36594349+xhable1337@users.noreply.github.com>
2020-12-03 17:02:05 +0300

    Update lab11.1.c

--- lab11.1.c
+++ lab11.1.c
@@ -3,36 +3,30 @@
 #include <locale.h>
 #include <math.h>
 
-/* Лабораторная работа 12.1 (использование рекурсии)
-Одноклеточная амеба каждые три часа делится на две клетки.
-Определить, сколько клеток будет через 3, 6, 9, … 24 часа.
-*/
-
 void sp() {
 	printf("------------------------------------------------\n");
 }
 
-int part_division(int time, int part_count, int end_time, int logging, int recursion_depth) {
-	if (logging == 1) { 
-		sp(); 
-		printf_s("[Глубина рекурсии: %d] Через %d часа (ов) количество клеток амёбы будет: %d \n", 
-			recursion_depth, time, part_count); 
-	}
-	if (time >= end_time || time + 3 > end_time) return part_count;
-	return part_division(time + 3, part_count * 2, end_time, logging, recursion_depth + 1);
-}
-
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int part_count = 1, hour_count = 24, recursion_depth = 0;
-	int logging = 1; // Вывод глубины рекурсивной функции и её результатов на текущий момент (0/1 - выкл/вкл)
+	int n; double x, sum = 0, add = 0;
 
-	printf_s("Введите количество часов: "); scanf_s("%d", &hour_count);
-
-	part_count = part_division(0, part_count, hour_count, logging, recursion_depth);
+	//const double e = 2.71828;
 
 	sp();
-	printf_s("Конечное количество клеток амёбы: %d\n", part_count);
+	printf_s("Введите x: "); scanf_s("%lf", &x);
 	sp();
+	printf_s("Введите n: "); scanf_s("%d", &n);
+	sp();
+
+	for (double i = 1; i <= n; i++)
+	{
+		add = pow(-1, 1 - i) * 2 * log(x);
+		//add = i * 2 * log(x);
+		sum += add;
+		printf_s("К сумме добавлено число: %lf\nТеперь сумма равна %lf\n", add, sum);
+		sp();
+	}
+	printf_s("Сумма = %lf", sum);
 	return 0;
 }
