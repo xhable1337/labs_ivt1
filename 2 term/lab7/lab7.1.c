56113614b3efac884c0b61d4cd24c4c90482dca0
xhable1337 <xhable.1995@gmail.com>
2021-04-25 04:57:41 +0300

    [lab7] Refactoring

--- 2 term/lab7/lab7.1.c
+++ 2 term/lab7/lab7.1.c
@@ -15,7 +15,42 @@ void sl() {
 	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 }
 
+// ============== Прототипы функций ==============
+void* Malloc(size_t size);
+void* Realloc(void* block, size_t size);
+void Init(size_t* col, size_t* row);
+void arrayInput(double* arr, size_t col, size_t row);
+void arrayRandom(double* arr, size_t col, size_t row);
+double arrayProccessing(double* array, size_t col, size_t row);
+void arrayToScreen(double* arr, size_t col, size_t row, double max_sum);
+void input(double* arr, size_t col, size_t row);
+// ===============================================
 
+int main() {
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	system("cls");
+
+	srand(time(NULL));
+
+	size_t col, row;
+
+	double* arr;
+
+	Init(&col, &row);
+
+	arr = (double*)Malloc(col * row * sizeof(double));
+
+	input(arr, col, row);
+
+	arrayToScreen(arr, col, row, arrayProccessing(arr, col, row));
+
+	system("pause");
+
+	return 0;
+}
+
+// Выделяет память размером size.
 void* Malloc(size_t size) {
 	void* point = malloc(size);
 
@@ -26,6 +61,7 @@ void* Malloc(size_t size) {
 	return point;
 }
 
+// Перевыделяет память на block размером size.
 void* Realloc(void* block, size_t size) {
 	void* point = realloc(block, size);
 
@@ -34,11 +70,10 @@ void* Realloc(void* block, size_t size) {
 	}
 
 	return point;
-
 }
 
+// Получает размерность массива col и row.
 void Init(size_t* col, size_t* row) {
-
 	int string, column;
 	sp();
 	do {
@@ -52,12 +87,10 @@ void Init(size_t* col, size_t* row) {
 	} while (string <= 0 || column <= 0);
 
 	*row = string; *col = column;
-
 }
 
 // Заполняет массив arr размерностью col x raw с клавиатуры.
-void input(double* arr, size_t col, size_t row) {
-
+void arrayInput(double* arr, size_t col, size_t row) {
 	int i, j;
 
 	for (i = 0; i < col; i++)
@@ -72,9 +105,36 @@ void input(double* arr, size_t col, size_t row) {
 
 }
 
+// Осуществляет пользовательский выбор способа заполнения массива arr размерностью col x row.
+void input(double* arr, size_t col, size_t row) {
+	int choice = 0;
+
+	do
+	{
+		printf_s("Способы ввода массива:\n"
+			"1) Ввод с клавиатуры\n"
+			"2) Заполнение с помощью псевдо-рандома\n\n"
+			"Каким способом вы желаете заполнить массив? ");
+		scanf_s("%d", &choice);
+		if (choice < 1 || choice > 2) printf_s("[!] Неправильный выбор! Число должно быть равно 1 или 2.\n\n");
+	} while (choice < 1 || choice > 2);
+	
+	switch (choice)
+	{
+	case 1:
+		arrayInput(arr, col, row);
+		break;
+	case 2:
+		arrayRandom(arr, col, row);
+		break;
+	default:
+		printf_s("[!] Произошла ошибка в switch.\n\n");
+		exit(500);
+	}
+}
+
 // Выводит массив arr размерностью col x row и максимальную сумму среди строк на экран.
 void arrayToScreen(double* arr, size_t col, size_t row, double max_sum) {
-
 	int i, j;
 	sl();
 
@@ -88,18 +148,16 @@ void arrayToScreen(double* arr, size_t col, size_t row, double max_sum) {
 
 	sp();
 
-	printf("Максимальная сумма элементов строк: %lf", max_sum);
-
+	printf("\nМаксимальная сумма элементов строк: %.3lf.\n", max_sum);
 }
 
 // Заполняет массив arr размерностью col x row псевдослучайными числами.
 void arrayRandom(double* arr, size_t col, size_t row) {
-
 	int i, j;
 
 	for (i = 0; i < col; i++) {
 		for (j = 0; j < row; j++) {
-			arr[i * row + j] = rand() % MAX_VALUE + 1;
+			arr[i * row + j] = rand() % MAX_VALUE + 1.0;
 		}
 	}
 }
@@ -119,29 +177,4 @@ double arrayProccessing(double* array, size_t col, size_t row) {
 	}
 
 	return max_sum;
-}
-
-int main() {
-	setlocale(LC_ALL, "Russian");
-	system("chcp 1251");
-	system("cls");
-
-	srand(time(NULL));
-
-	size_t col, row;
-
-	double* arr;
-
-	Init(&col, &row);
-
-	arr = (double*)Malloc(col * row * sizeof(double));
-
-	//input(Array,col,row);
-	arrayRandom(arr, col, row);
-
-	arrayToScreen(arr, col, row, arrayProccessing(arr, col, row));
-
-	system("pause");
-
-	return 0;
 }
---
+++ 2 term/lab7/lab7.2.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <malloc.h>
+#include <limits.h>
+#include <math.h>
+#include <time.h>
+
+#define MAX_VALUE 1000 
+
+void sp() {
+	printf("------------------------------------------------\n");
+}
+void sl() {
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+}
+
+// ============== Прототипы функций ==============
+void* Malloc(size_t size);
+void* Realloc(void* block, size_t size);
+void Init(size_t* row);
+void arrayToScreen(int** arr, size_t row);
+double arrayProccessing(int** arr, size_t row, double* arith_mean, int* mark_count, int* bad_mark, int* good_st);
+double inputMarks(int** arr, size_t row);
+// ===============================================
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	system("cls");
+
+	srand(time(NULL));
+
+	size_t col = 1, row = 1;
+
+	Init(&row);
+	// Массив с колличеством студентов
+	int** array = (int**)Malloc(row * sizeof(int*));
+
+	inputMarks(array, row);
+	arrayToScreen(array, row);
+
+	double arith_mean;
+	int mark_count;
+	int bad_mark;
+	int good_st;
+
+	arrayProccessing(array, row, &arith_mean, &mark_count, &bad_mark, &good_st);
+
+	printf_s("Количество оценок: %d\nКоличество неуд. оценок: %d\nКолличество отличников: %d\nСредний балл: %lf\n", mark_count, bad_mark, good_st, arith_mean);
+
+	//system("pause");
+
+	return 0;
+}
+
+// Выделяет память размером size.
+void* Malloc(size_t size) {
+	void* point = malloc(size);
+
+	if (!point) {
+		printf("Недостаточно памяти!\n"); exit(1);
+	}
+
+	return point;
+}
+
+// Перевыделяет память на block размером size.
+void* Realloc(void* block, size_t size) {
+	void* point = realloc(block, size);
+
+	if (!point) {
+		printf("Недостаточно памяти!\n"); exit(1);
+	}
+
+	return point;
+}
+
+// Получает размерность массива структуры студентов (row).
+void Init(size_t* row) {
+	int string;
+	int column;
+	sp();
+	do {
+		printf("Введите количество студентов: "); scanf_s("%d", &string);
+		if (string <= 0)
+		{
+			printf_s("[!] Неправильный ввод размерности массива. Повторите ввод.\n");
+			sp();
+		}
+	} while (string <= 0);
+
+	*row = string;
+
+}
+
+// Выводит массив студентов arr размерностью row на экран.
+void arrayToScreen(int** arr, size_t row) {
+	int i, j;
+	size_t col = 0;
+	int init = 0;
+
+	sl();
+
+	for (i = 0; i < row; i++) {
+		printf("Студент № %d:\n", i);
+
+		if (init == 0) {
+			col = arr[i][0];
+			init = 1;
+		}
+
+		for (j = 1; j < col; j++) { printf("|%2d ", arr[i][j]); }
+
+		printf("|\n");
+
+		init = 0;
+	}
+
+	sl();
+}
+
+// Находит в массиве arr размерностью arr средний балл по предмету, количество неуд. оценок и количество отличников.
+double arrayProccessing(int** arr, size_t row, double* arithMean, int* markCount, int* badMarks, int* goodStudents) {
+	int i, j;
+	size_t col = 0;
+	int init = 0;
+
+	*arithMean = 0;
+	*markCount = 0;
+	*badMarks = 0;
+	*goodStudents = 0;
+
+	for (i = 0; i < row; i++) {
+		int good = 1;
+		if (init == 0) {
+			col = arr[i][0];
+			init = 1;
+		}
+
+		for (j = 1; j < col; j++) {
+			*markCount += 1;
+
+			if (arr[i][j] < 3)
+				*badMarks += 1;
+
+			if (arr[i][j] != 5) 
+				good = 0;
+
+			*arithMean += arr[i][j];
+		}
+
+		if (good) *goodStudents += 1;
+
+		init = 0;
+	}
+
+	*arithMean /= *markCount;
+}
+
+// Получает оценки студентов с клавиатуры и записывает их в массив arr размерностью row.
+double inputMarks(int** arr, size_t row) {
+	int i, j, mark;
+
+	int counter = 0, input;
+
+	int* values = NULL;
+	int* many_numbers;
+
+	for (i = 0; i < row; i++) {
+		values = NULL;
+		counter = 1;
+
+		do
+		{
+			counter++;
+			int check = 0;
+			do {
+
+				printf("Оценка [%d] студента № %d: ", counter - 1, i); scanf_s("%d", &input);
+				if (input < 0 || input > 5) {
+					printf("Неверное значение оценки\n");
+					check = 0;
+				}
+				else check = 1;
+			} while (!check);
+
+			// при добавлении нового числа, увеличиваем массив на 1
+			many_numbers = (int*)Realloc(values, counter * sizeof(int));
+			// записываем колличество оценок студента в нулевой элмент массива
+			many_numbers[0] = counter - 1;
+
+			if (many_numbers != NULL)
+			{
+				values = many_numbers;
+				values[counter - 1] = input;                      // добавить к массиву только что введённое число
+			}
+			else
+			{
+				free(values);                                   // удалить массив
+				printf("Ошибка перевыделения памяти!");
+				exit(1);                                        // завершить работу программы
+			}
+		} while (input != 0);
+
+		arr[i] = values;
+
+		sl();
+	}
+}
